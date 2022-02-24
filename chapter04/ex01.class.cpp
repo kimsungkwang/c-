8e886c0e9ca7842eed3fc4a5ebec92f6cd05e019
kimsungkwang <skk405@naver.com>
2022-02-24 17:31:51 +0900

    04

---
+++ chapter04/ex01.class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+class Circle {
+    public:
+        int radius;
+        string color;
+
+        double calcArea() {
+            return 3.14 * radius * radius;
+        }
+};
+
+
+int main(int argc, char const *argv[])
+{
+   Circle obj;
+   obj.radius = 100;
+   obj.color = "blie";
+
+   cout << "area : " << obj.calcArea() << endl;
+   return 0;
+}
