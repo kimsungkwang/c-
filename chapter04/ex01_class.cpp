1f65a985d8049580342d7817ff0b330318210986
kimsungkwang <skk405@naver.com>
2022-02-25 09:24:27 +0900

    04

--- chapter04/ex01_class.cpp
+++ chapter04/ex01_class.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 #include "Circle.h"       // 현재 디렉토리에서 먼저 찾아라
-// #include <Circle.cpp>       // 라이브러리 디렉토리에서 찾아라
+// #include <Circle.h>       // 라이브러리 디렉토리에서 찾아라
 
 int main(int argc, char const *argv[])
 {
